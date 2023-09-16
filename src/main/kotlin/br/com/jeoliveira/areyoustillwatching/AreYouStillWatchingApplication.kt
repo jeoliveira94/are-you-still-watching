860ef31d31f435f77ee2b1af87ac223428adf846
José Oliveira <eduardo.jeso@gmail.com>
2023-09-16 12:53:23 -0300

    first commit

---
+++ src/main/kotlin/br/com/jeoliveira/areyoustillwatching/AreYouStillWatchingApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jeoliveira.areyoustillwatching
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AreYouStillWatchingApplication
+
+fun main(args: Array<String>) {
+    runApplication<AreYouStillWatchingApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jeoliveira/areyoustillwatching/AreYouStillWatchingApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jeoliveira.areyoustillwatching
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AreYouStillWatchingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
