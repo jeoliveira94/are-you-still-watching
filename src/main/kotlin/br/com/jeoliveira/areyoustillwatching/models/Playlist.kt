b014c7d2f2d728fe258727f527e4461488f23ed7
José Oliveira <eduardo.jeso@gmail.com>
2023-09-19 14:12:45 -0300

    [improvement] Update table name for standardization

--- src/main/kotlin/br/com/jeoliveira/areyoustillwatching/models/Playlist.kt
+++ src/main/kotlin/br/com/jeoliveira/areyoustillwatching/models/Playlist.kt
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 @Entity
-@Table(name = "playlists")
+@Table(name = "playlist")
 class Playlist(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
